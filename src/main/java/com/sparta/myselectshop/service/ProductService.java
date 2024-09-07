dcc9b47c0fbd318f2780f46a47dcb09eb214d60f
MOON <moonjs1018@naver.com>
2024-09-08 02:00:36 +0900

    상품 폴더 설계

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -48,6 +48,7 @@ public class ProductService {
         return new ProductResponseDto(product);
     }
 
+    @Transactional(readOnly = true)
     public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy, boolean isAsc) {
 
         Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
@@ -63,8 +64,6 @@ public class ProductService {
         } else {
             productList = productRepository.findAll(pageable);
         }
-
-        List<ProductResponseDto> responseDtoList = new ArrayList<>();
 
         return productList.map(ProductResponseDto::new);
     }
